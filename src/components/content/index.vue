551b47818ccd0c5a97ac8e03981a24708173b645
yjf <1374993958@qq.com>
2020-02-14 14:49:36 +0800

    2020/2/14项目前端搭建

---
+++ src/components/content/index.vue
@@ -0,0 +1,68 @@
+<template>
+    <el-container>
+        <el-header>Header</el-header>
+        <el-container>
+            <el-aside width="200px">
+                <el-menu
+                        default-active="1"
+                        class="el-menu-vertical-demo"
+                        @open="handleOpen"
+                        @close="handleClose"
+                        background-color="#545c64"
+                        text-color="#fff"
+                        active-text-color="#ffd04b"
+                        router
+                >
+                    <el-submenu index="1">
+                        <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span>导航一</span>
+                        </template>
+                        <el-submenu index="/index">
+                            <template slot="title">部门员工管理</template>
+                            <el-menu-item index="/index/dept">
+                                <i class="el-icon-location"></i>
+                                <span>部门信息管理</span>
+                            </el-menu-item>
+                            <el-menu-item index="/index/emp">
+                                <i class="el-icon-location"></i>
+                                <span>员工信息管理</span>
+                            </el-menu-item>
+                        </el-submenu>
+                    </el-submenu>
+                </el-menu>
+            </el-aside>
+            <el-main>
+                <router-view></router-view>
+            </el-main>
+        </el-container>
+    </el-container>
+</template>
+
+<script>
+    export default {
+        methods: {
+            handleOpen(key, keyPath) {
+                console.log(key, keyPath);
+            },
+            handleClose(key, keyPath) {
+                console.log(key, keyPath);
+            }
+        }
+    }
+</script>
+<style>
+    .el-header{
+        background-color: #545c64;
+    }
+    .el-aside {
+        background-color: #545c64;
+    }
+
+    .el-main {
+        background-color: #E9EEF3;
+    }
+    .el-container {
+        height: 700px;
+    }
+</style>
---
+++ src/components/content/login.vue
@@ -0,0 +1,46 @@
+<template>
+    <el-dialog title="收货地址" :show-close="false" :visible.sync="dialogFormVisible">
+        <el-form >
+            <el-form-item label="用户名" :label-width="formLabelWidth">
+                <el-input v-model="user.uname" ></el-input>
+            </el-form-item>
+            <el-form-item label="密码" :label-width="formLabelWidth">
+                <el-input v-model="user.account" ></el-input>
+            </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+            <el-button  type="primary" @click="login">确 定</el-button>
+        </div>
+    </el-dialog>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                user:{
+                    uname: '',
+                    account: ''
+                },
+                dialogFormVisible: true,
+                formLabelWidth: '120px',
+            };
+        },
+        methods:{
+            login(){
+                if(this.user.uname=='' || this.user.account=='' ){
+                    this.$message.error('请输入完整');
+                }
+                console.log(this.user.uname);
+                console.log(this.user.account);
+                
+                if(this.user.uname!='' && this.user.account!='' ){
+                    this.$message.success('success');
+                    window.sessionStorage.setItem("uname",this.user.uname);
+                    this.$router.push("/index");
+                }
+                this.dialogFormVisible=false
+            }
+        }
+    };
+</script>
